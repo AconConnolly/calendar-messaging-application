029a7a79284aa5baba8576bf77f5ffa2bb08d598
MyYodaSoda <alexander.connolly@edu.sait.ca>
2023-12-04 08:29:42 -0700

    Trimmming bloat

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,11 @@ import {
   useLocation,
 } from "react-router-dom";
 import Calendar from "./Components/Pages/CalendarComponents/Calendar";
-import Wallet from "./Components/Pages/Wallet";
-import TechSupport from "./Components/Pages/UserHelp/TechSupport";
 import Register from "./Components/Register";
-import Settings from "./Components/Pages/Setting Components/Settings";
-import ContactUs from "./Components/Pages/UserHelp/ContactUs";
-import Confirm from "./Components/Pages/UserHelp/Confirm";
 import Profile from "./Components/Pages/UserProfile/Profile";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import ChatPage from "./Components/Pages/ChatPage/ChatPage";
-import Welcome from "./Components/Pages/WelcomePage/Welcome";
-
 import ForgotPassword from "./Components/Pages/ForgotPassword";
 // function App() {
 //   return (
@@ -56,21 +49,11 @@ function App(props) {
   } else {
     return (
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="/calender" element={<Calendar />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/wallet" element={<Wallet />} />
-        {/* <Route path="/messaging" element={<Messaging />} /> */}
-
-        <Route path="/tech-support" element={<TechSupport />} />
         <Route path="/reg" element={<Register />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/confirm" element={<Confirm />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/profile" element={<Profile />} />
-
         <Route path="/reset" element={<ForgotPassword />} />
       </Routes>
     );
